d1f82555c8c30220de82d13275ea2837e93d8de6
viralkansodiya <viral@fosserp.com>
2024-09-25 16:51:53 +0000

    ch

--- certification/shop_management/doctype/payment_request/payment_request.py
+++ certification/shop_management/doctype/payment_request/payment_request.py
@@ -1,9 +1,69 @@
 # Copyright (c) 2024, viral and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
+import calendar
+from frappe.utils import getdate
 
 
 class PaymentRequest(Document):
 	pass
+
+
+
+def notification_payment_reminder():
+	rent_agreements = frappe.db.sql(f"""
+						Select name, shop_owner, monthly_rent_payment_date, enable_payment_reminder
+						From `tabRent Agreement`
+						Where status = 'Active'
+					""", as_dict = 1)
+					
+	for row in rent_agreements:
+		if getdate().month == (row.monthly_rent_payment_date - 2) and row.enable_payment_reminder:
+			content = ''
+			due_payment = frappe.db.sql(f"""
+								Select name, posting_date, rent_amount, lessee_shop_owner
+								From `tabPayment Request`
+								Where rent_agreement = '{row.name}' and status = 'Unpaid'
+						""", as_dict = 1)
+			
+			content += """
+						<h2>Gental Reminder of Rent Payment</h2>
+
+						<p> Hello, {0} </p>
+
+						<p>Bellow is the detail about your due payment of rent</p>
+						""".format(frappe.db.get_value("Lessee - Shop Owner", row.shop_owner, "owner_name"))
+
+			content += """
+						<table width='60%' border=1>
+							<tr>
+								<th>Month</th>
+								<th>Rent Amount</th>
+							</tr>
+						"""
+			for d in due_payment:
+				content += """
+						<tr>
+							<td align='center'>
+								{0}
+							</td>
+							<td align='center'>
+								{1}
+							</td>
+						</tr>
+					""".format(calendar.month_name[d.posting_date.month], d.rent_amount)
+				
+			content += "</table>"
+			email_id = frappe.db.get_value("Lessee - Shop Owner", row.shop_owner, "email_id")
+			
+			frappe.sendmail(
+					recipients=email_id,
+					subject="Rent Payment Reminder",
+					message=content
+				)
+		
+
+
+
--- certification/shop_management/doctype/rent_agreement/rent_agreement.py
+++ certification/shop_management/doctype/rent_agreement/rent_agreement.py
@@ -17,8 +17,12 @@ class RentAgreement(Document):
 				""", as_dict=1)
 		if(len(data)):
 			frappe.throw("Rent Agreement Already booked for {1} to {2}, <b>{0}</b>".format(get_link_to_form("Rent Agreement", data[0].get('name')), data[0].get('start_date'), data[0].get('end_date')))
+
+		frappe.db.set_value("Shop Detail", self.shop_detail , "status", "Booked")
+
 	def on_cancel(self):
 		self.status = "Cancelled"
+		frappe.db.set_value("Shop Detail", self.shop_detail , "status", "Available")
 
 
 @frappe.whitelist()
@@ -42,8 +46,8 @@ def make_payment_receipt(source_name, target_doc=None):
 def update_agreement_status():
 	current_date = str(getdate())
 	frappe.db.sql(f"""
-				Update `tabRent Agreement` SET status = 'Expired'
-				Where status = 'Active' and docstatus=1 and end_date < '{current_date}'
+				Update `tabRent Agreement` as ra, `tabShop Detail` as sd SET ra.status = 'Expired', sd.status = 'Available'
+				Where ra.status = 'Active' and ra.docstatus=1  and ra.status != 'Cancelled' and  ra.shop_detail = sd.name and ra.end_date < '{current_date}'
 			""", as_dict = 1)
 	
 
@@ -51,7 +55,14 @@ def update_agreement_status():
 def create_payment_request():
 	current_date = str(getdate())
 	rent_agreement = frappe.db.sql(f"""
-			Select name , shop_owner, company_name, shop_no, standard_rate, monthly_rent,monthly_rent_payment_date, end_date, shop_detail
+			Select name , 
+				shop_owner, 
+				company_name, 
+				shop_no, 
+				standard_rate, 
+				monthly_rent,
+				monthly_rent_payment_date, 
+				end_date, shop_detail
 			From `tabRent Agreement`
 			where docstatus = 1 and status = 'Active'
 	""", as_dict=1)
@@ -71,6 +82,8 @@ def create_payment_request():
 			new_doc.submit()
 		except Exception as e:
 			frappe.log_error(e)
+		
+
 
 
 	
